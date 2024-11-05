c17d24418740e273b4ec26aa006f7e7a0b5ad597
Mara Backsen <mbacksen2004@gmail.com>
2024-11-05 10:41:42 -0700

    Adding basic discover page

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -20,3 +20,10 @@ CREATE TABLE user_rankings (
     restaurant_id INTEGER REFERENCES restaurants(id),
     rank INTEGER NOT NULL
 );
+
+/* for discover, I'm trying to access these variables for restaurants (hoping you can include them in the databse):
+- name
+- image
+- rating 
+- website
+- button for more details? (maybe all the reviews, rankings, etc.)
